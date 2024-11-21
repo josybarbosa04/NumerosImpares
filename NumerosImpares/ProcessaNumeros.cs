3098497a5d20902987393f8103f169de27dac816
josybarbosa04 <josenilda_silva123@outlook.com>
2024-11-21 12:41:01 -0300

    Adicionar arquivos de projeto.

---
+++ NumerosImpares/ProcessaNumeros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosImpares
+{
+    public class ProcessaNumeros
+    {
+        public List<int> ObterNumImpar(int[] numeros)
+        {
+            return numeros.Where(n => n % 2 != 0).ToList();
+        }
+    }
+}
---
+++ NumerosImpares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosImpares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessaNumeros processaNumeros = new ProcessaNumeros();
+
+            int[] numeros = {2,4,6,8,9,11,12,14,16,33};
+
+            List<int> listImpares = processaNumeros.ObterNumImpar(numeros);
+
+            Console.WriteLine("Números ímpares: ");
+            foreach (int n in listImpares) 
+            { 
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
